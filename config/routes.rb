7ee2fce0630f35f049aadeca0555aaf607094350
chandni2424 <chandnissirohi@gmail.com>
2020-08-15 23:01:50 +0530

    Eventz App Complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  resources :categories
+  root "events#index"
+
+  get "events/filter/:filter" => "events#index", as: :filtered_events
+
+  resources :events do
+    resources :registrations
+    resources :likes
+  end
+
+  resource :session, only: [:new, :create, :destroy]
+
+  resources :users
+  get "signup" => "users#new"
 end
